15df88d820ed195aca7a111440bb21302ad2d4c8
zaphodbb <mullens101@gmail.com>
2022-11-28 22:43:24 -0500

    Rewrite 11/22

--- README.md
+++
@@ -1,3 +0,0 @@
-# lightshow
-Python code to run lightshow on RPi Model 3B with ws2812b LED strips. Requires Adafruit Neopixel library https://github.com/jgarff/rpi_ws281x.
-
---
+++ bin/presets.py
@@ -0,0 +1,105 @@
+RAINBOW = {"dur": 3600,
+           "show": "Rainbow",
+           "args": {"speed": 0.01,
+                    "period": 2
+                   }
+          }
+
+RAINBOWCOMET = {"dur": 3600,
+                "show": "RainbowComet",
+                "args": {"speed": 0.001,
+                         "tail_length": 60,
+                         "bounce": True,
+                         "skip": 10
+                        }
+               }
+
+RAINBOWCHASE = {"dur": 3600,
+                "show": "RainbowChase",
+                "args": {"speed": 0.001,
+                         "size": 5,
+                         "spacing": 5,
+                         "step": 8
+                        }
+               }
+
+USA = {"dur": 3600,
+       "show": "CustomColorChase",
+       "args": {"speed": 0.01,
+                "size": 5,
+                "spacing": 5,
+                "colors": ["RED",
+                           "WHITE",
+                           "BLUE"]
+               }
+      }
+
+HALLOWEEN = {"dur": 3600,
+             "show": "CustomColorChase",
+             "args": {"speed": 0.01,
+                      "size": 5,
+                      "spacing": 5,
+                      "colors": ["PURPLE",
+                                 "ORANGE",
+                                ]
+                     }
+            }
+
+CHRISTMAS = {"dur": 3600,
+             "show": "ColorCycle",
+             "args": {"speed": 0.1,
+                      "colors": [(255,0,0),
+                                 (225,0,0),
+                                 (200,0,0),
+                                 (175,0,0),
+                                 (150,0,0),
+                                 (125,0,0),
+                                 (100,0,0),
+                                 (75,0,0),
+                                 (50,0,0),
+                                 (25,0,0),
+                                 (0,0,0),
+                                 (0,25,0),
+                                 (0,50,0),
+                                 (0,75,0),
+                                 (0,100,0),
+                                 (0,125,0),
+                                 (0,150,0),
+                                 (0,175,0),
+                                 (0,200,0),
+                                 (0,225,0),
+                                 (0,255,0),
+                                 (0,225,0),
+                                 (0,200,0),
+                                 (0,175,0),
+                                 (0,150,0),
+                                 (0,125,0),
+                                 (0,100,0),
+                                 (0,75,0),
+                                 (0,50,0),
+                                 (0,25,0),
+                                 (0,0,0),
+                                 (25,0,0),
+                                 (50,0,0),
+                                 (75,0,0),
+                                 (100,0,0),
+                                 (125,0,0),
+                                 (150,0,0),
+                                 (175,0,0),
+                                 (200,0,0),
+                                 (225,0,0)
+                                ]
+                     }
+            }
+
+STPATRICKSDAY = {"dur": 3600,
+                 "show": "CustomColorChase",
+                 "args": {"speed": 0.01,
+                          "size": 5,
+                          "spacing": 5,
+                          "colors": ["GREEN",
+                                     "WHITE",
+                                    ]
+                         }
+                }
+
